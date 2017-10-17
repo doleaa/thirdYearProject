385c48687a09c09db4bfaf54d96ffd2872c8924f
alex <alexandru.dolea@student.manchester.ac.uk>
2017-10-17 08:40:16 +0100

    First take at Front End

--- backEnd/src/main/java/com/dolea/backEnd/dropwizard/SQLInterfaceBackEndApplication.java
+++ backEnd/src/main/java/com/dolea/backEnd/dropwizard/SQLInterfaceBackEndApplication.java
@@ -10,7 +10,11 @@ import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 import java.util.List;
 
 /**
@@ -32,6 +36,7 @@ public class SQLInterfaceBackEndApplication extends Application<Configuration> {
     @Override
     public void run(Configuration configuration,
                     final Environment environment) {
+        configureCors(environment);
         Injector injector = Guice.createInjector(new SQLInterfaceBackEndModule());
 
         List<Class<?>> resourceClasses = PackageScanner.getClassesInPackage("com.dolea.backEnd.resources");
@@ -40,4 +45,20 @@ public class SQLInterfaceBackEndApplication extends Application<Configuration> {
                 .filter(resourceClass -> resourceClass.getSuperclass().equals(BaseBackEndResource.class))
                 .forEach(resourceClass -> environment.jersey().register(injector.getInstance(Key.get(resourceClass))));
     }
+
+    private void configureCors(Environment environment) {
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+    }
+
 }
