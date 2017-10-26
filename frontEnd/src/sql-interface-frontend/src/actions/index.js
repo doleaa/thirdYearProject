ca8486f8f39b61b4854cef33c15f927e822ce0a5
alex <alexandru.dolea@student.manchester.ac.uk>
2017-10-26 18:59:43 +0100

    Executor Component:
    -Sync action creators
    -Thunk action creator
    -reducer
    -thunkMiddleware

--- frontEnd/src/sql-interface-frontend/src/actions/index.js
+++ frontEnd/src/sql-interface-frontend/src/actions/index.js
@@ -1,3 +1,5 @@
+import $ from 'jquery'
+
 export const setMode = mode => {
     return {
         type: "SET_MODE",
@@ -29,12 +31,31 @@ export const executeQuery = query => {
     return dispatch => {
         dispatch(initiateExecution())
 
-        const dbMap = {}
+        const dbMap = {
+            "url": "jdbc:h2:/tmp/bam/test;AUTO_SERVER=TRUE",
+            "username": "sa",
+            "password": ""
+        }
 
         const requestBody = {
-            sqlCommand: query,
-            dbMap
+            "sqlCommand" : query,
+            "dbMap": dbMap,
+            "comments": ""
         }
-        dispatch(interpretResponse({status: 200, text: () => (null)}))
+
+        const http = new XMLHttpRequest()
+        const url = "http://127.0.0.1:8090/command"
+        const method = "POST"
+
+        http.open(method, url, true)
+        http.setRequestHeader("Content-type", "application/json")
+
+        http.onreadystatechange = () => {
+            if ( http.status === 200 ) {
+                dispatch(interpretResponse(JSON.parse(http.responseText)))
+            }
+        }
+
+        http.send(JSON.stringify(requestBody))
     }
 }
